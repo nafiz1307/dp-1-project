1cee292fd7f810436c36f7c9e31fd37bf618bc62
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2021-08-25 20:48:24 +0600

    implemented product detail

--- dp-ecom-final/src/Routes.js
+++ dp-ecom-final/src/Routes.js
@@ -3,6 +3,7 @@ import { Web3Context } from './Context/Web3Context'
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from './pages/Home';
 import Shop from './pages/Shop';
+import ShopDetail from './pages/ShopDetail'
 
 export default function Routes() {
     const [web3Context, setWeb3Context] = useState({})
@@ -12,6 +13,7 @@ export default function Routes() {
             <Web3Context.Provider value={{web3Context, setWeb3Context}}>
                 <Route exact path = "/" component = {Home}/>
                 <Route exact path = "/shop" component = {Shop}/>
+                <Route exact path = "/shop-detail" component = {ShopDetail}/>
             </Web3Context.Provider>
         </Switch>
         </BrowserRouter>
---
+++ dp-ecom-final/src/pages/ShopDetail.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+// import Body from '../components/Body'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+// import Humberger from '../components/Humberger'
+import PagePreloader from '../components/Page-preloader'
+import HeroSection from '../components/HeroSection'
+import ProductDetail from '../components/ProductDetail'
+
+export default function Home(){
+    return(
+        <Fragment>
+            <PagePreloader />
+            <Header />
+            <HeroSection/>
+            <ProductDetail/>
+            <Footer />
+        </Fragment>
+    )
+}
